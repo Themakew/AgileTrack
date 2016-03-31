eb074b9d24fcc3626d49148b87baadb7108fe3cb
Ruyther Costa <ruy_fusion@hotmail.com>
2016-03-30 23:09:39 -0300

    Login working partially

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,7 +1,26 @@
+import agiletrack.Role
+import agiletrack.User
+import agiletrack.UserRole
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+   def init = {
+
+      def adminRole = new Role('ROLE_ADMIN').save()
+      def userRole = new Role('ROLE_USER').save()
+
+      def testUser = new User('root', 'root').save()
+
+      UserRole.create testUser, adminRole
+
+      UserRole.withSession {
+         it.flush()
+         it.clear()
+      }
+
+      assert User.count() == 1
+      assert Role.count() == 2
+      assert UserRole.count() == 1
+   }
 }
+
